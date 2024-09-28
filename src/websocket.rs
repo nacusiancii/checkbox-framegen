7ef0cb6bd56a80a5f5725fb261d791e3fd3ca71d
nacusiancii <146810142+nacusiancii@users.noreply.github.com>
2024-09-28 19:13:51 +0530

    Update websocket.rs

--- src/websocket.rs
+++ src/websocket.rs
@@ -1,5 +1,6 @@
 use axum::{
-    extract::{ws::{Message, WebSocket, WebSocketUpgrade}, State},
+    extract::ws::{Message, WebSocket, WebSocketUpgrade},
+    extract::State,
     response::IntoResponse,
 };
 use futures::{SinkExt, StreamExt};
@@ -13,25 +14,23 @@ pub async fn ws_handler(
     ws.on_upgrade(|socket| handle_socket(socket, state))
 }
 
-async fn handle_socket(socket: WebSocket, state: Arc<Mutex<AppState>>) {
-    let (mut sender, mut receiver) = socket.split();
-
+async fn handle_socket(mut socket: WebSocket, state: Arc<Mutex<AppState>>) {
     let mut rx = {
         let state = state.lock().unwrap();
         state.tx.subscribe()
     };
 
     // Send initial I-frame
-    send_initial_frame(&mut sender, &state).await;
+    send_initial_frame(&mut socket, &state).await;
 
     // Handle incoming messages (including reconnection requests)
     let state_clone = state.clone();
-    let incoming = tokio::spawn(async move {
-        while let Some(Ok(message)) = receiver.next().await {
+    let mut recv_task = tokio::spawn(async move {
+        while let Some(Ok(message)) = socket.recv().await {
             match message {
                 Message::Text(text) => {
                     if let Ok(client_msg) = serde_json::from_str::<ClientMessage>(&text) {
-                        handle_client_reconnect(&mut sender, &state_clone, client_msg.last_version).await;
+                        handle_client_reconnect(&mut socket, &state_clone, client_msg.last_version).await;
                     }
                 }
                 Message::Close(_) => break,
@@ -41,10 +40,10 @@ async fn handle_socket(socket: WebSocket, state: Arc<Mutex<AppState>>) {
     });
 
     // Stream frames to the client
-    let outgoing = tokio::spawn(async move {
+    let mut send_task = tokio::spawn(async move {
         while let Ok(frame) = rx.recv().await {
             let message = Message::Binary(bincode::serialize(&frame).unwrap());
-            if sender.send(message).await.is_err() {
+            if socket.send(message).await.is_err() {
                 break;
             }
         }
@@ -52,25 +51,25 @@ async fn handle_socket(socket: WebSocket, state: Arc<Mutex<AppState>>) {
 
     // Wait for either incoming or outgoing to finish
     tokio::select! {
-        _ = incoming => {},
-        _ = outgoing => {},
-    }
+        _ = (&mut recv_task) => send_task.abort(),
+        _ = (&mut send_task) => recv_task.abort(),
+    };
 }
 
-async fn send_initial_frame(sender: &mut futures::stream::SplitSink<WebSocket, Message>, state: &Arc<Mutex<AppState>>) {
+async fn send_initial_frame(socket: &mut WebSocket, state: &Arc<Mutex<AppState>>) {
     let state = state.lock().unwrap();
     let initial_frame = Frame::IFrame {
         version: state.version,
         timestamp: std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_secs(),
         data: state.state.clone().into_vec(),
     };
-    let _ = sender
+    let _ = socket
         .send(Message::Binary(bincode::serialize(&initial_frame).unwrap()))
         .await;
 }
 
 async fn handle_client_reconnect(
-    sender: &mut futures::stream::SplitSink<WebSocket, Message>,
+    socket: &mut WebSocket,
     state: &Arc<Mutex<AppState>>,
     client_version: usize,
 ) {
@@ -85,7 +84,7 @@ async fn handle_client_reconnect(
     if current_version - client_version > crate::MAX_CATCHUP_FRAMES || client_version > current_version {
         // Too many missed frames or client version is in the future, send full state
         drop(state); // Unlock the mutex before calling send_initial_frame
-        send_initial_frame(sender, state).await;
+        send_initial_frame(socket, state).await;
     } else {
         // Send missed P-frames
         let missed_frames: Vec<Frame> = state.frame_buffer
@@ -97,10 +96,9 @@ async fn handle_client_reconnect(
             .collect();
 
         for frame in missed_frames {
-            let _ = sender
+            let _ = socket
                 .send(Message::Binary(bincode::serialize(&frame).unwrap()))
                 .await;
         }
     }
 }
-
